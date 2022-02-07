313d4633cc449d879a756d3b37a1513487613f46
Hector Calles <hector7chess@gmail.com>
2022-02-07 13:23:03 -0600

    Continuacion prueba

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 const btnWallet = 
 document.addEventListener("DOMContentLoaded", function () {
     const btn = document.getElementById("btn-chivo");
-    btn.addEventListener("click", e => { e.preventDefault(); alert("Clickeado") });
+    btn.addEventListener("click", function(e){ e.preventDefault(); /**Funcion de callback */ registerPayment(); });
+
+    /**
+     * FN: Register payment
+     * IN: 
+     */
+    function registerPayment(){
+        let URL = "https://poke-benja-fd1142.netlify.app/";
+        window.open(URL, 'CHIVO PAGOS', 
+        'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=1,width=600,height=1000,left = 400,top = 50');
+    }
 });
+
+
 
 // module.exports = { btnWallet }
